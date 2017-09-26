0805cc4c4316ef3f11d2dabefd2b0d3111425bb8
DaviGuarana <daviguarana20@gmail.com>
2017-09-25 21:40:47 -0300

    oo

--- cabecalho.php
+++ cabecalho.php
@@ -8,7 +8,7 @@
 <ul>
   		<li><a class="active" href="index.php">Cadastre-se</a></li>
   		<li><a href="sobre.html">Sobre</a></li>
-  		<li><a href="#contact">Contact</a></li>
+  		<li><a href="#contact">Contatos</a></li>
   		<li><a href="#servicos">Serviços</a></li>
   		<li><a href="index.php">Home</a></li>
   		<li><a href="index.php" id="logo">SSexpress</a></li>
