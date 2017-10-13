6eff9ca0c563125c8c78fd167a5466260052fd70
DaviGuarana <daviguarana20@gmail.com>
2017-10-12 21:22:21 -0300

    Atualização na página do cadastro, no banco de dados e na página do usuário

--- javascript/cabecalho.js
+++ javascript/cabecalho.js
@@ -7,10 +7,6 @@ function closeNav() {
 }
 // Get the button, and when the user clicks on it, execute myFunction
 document.getElementById('myBtn').onclick = function() {myFunction()};
-<<<<<<< HEAD
-=======
-
->>>>>>> 8dfd6ef0b564aca8b63ac601a8af4b0c1c7056c8
 /* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
 function myFunction() {
   document.getElementById('myDropdown').classList.toggle('show');
